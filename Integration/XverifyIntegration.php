4fb99f94c803d3104236413c64d40e287461b532
Scott Shipman <scottshipman@Scotts-MacBook-Pro.local>
2018-01-23 14:17:29 -0500

    stubbed and started Xverify integration

---
+++ Integration/XverifyIntegration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: scottshipman
+ * Date: 1/23/18
+ * Time: 1:31 PM
+ */
+
+namespace MauticPlugin\MauticEnhancerBundle\Integration;
+
+
+class XverifyIntegration
+{
+
+}
