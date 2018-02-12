480cb0875c50a8c3022f060181208b8c7f6d3c09
Nicholai Bush <nbush@thedmsgrp.com>
2018-02-12 11:18:30 -0500

    most of skeleton functional

--- Integration/AgeFromBirthdateIntegration.php
+++ Integration/AgeFromBirthdateIntegration.php
@@ -4,5 +4,37 @@ namespace MauticPlugin\MauticEnhancerBundle\Integration;
 
 class AgeFromBirthdateIntegration extends AbstractEnhancerIntegration
 {
+    const INTEGRATION_NAME = 'AgeFromBirthdate';
     
+    public function getAuthenticationType()
+    {
+        return 'none';
+    }
+    
+    public function getName()
+    {
+        return self::INTEGRATION_NAME;
+    }
+    
+    public function getDisplayName()
+    {
+        return self::INTEGRATION_NAME . 'Age From Birthdate Data Enhancer';    
+    }
+
+    protected function getEnhancerFieldArray()
+    {
+        $settings = $this->getIntegrationSettings()->getFeatureSettings();
+        
+        return [
+             'age_from_dob' => [
+                'label' => 'Age from DoB'
+            ]
+        ];
+    }
+    
+    public function doEnhancement(Lead $lead)
+    {
+            $this->leadModel->saveEntity($lead);
+            $this->em->flush();
+    }
 }
