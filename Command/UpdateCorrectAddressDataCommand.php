5e5a965a2b689ea03c1609b36ff258390fe48d23
cykonetic <nicholaibush@yahoo.com>
2018-06-08 08:22:51 -0400

    [ENG-49] minor tweak

--- Command/UpdateCorrectAddressDataCommand.php
+++ Command/UpdateCorrectAddressDataCommand.php
@@ -29,7 +29,7 @@ class UpdateCorrectAddressDataCommand extends ModeratedCommand
      * @param InputInterface  $input
      * @param OutputInterface $output
      *
-     * @return int|null|void
+     * @return int
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
