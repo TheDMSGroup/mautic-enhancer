7f53fb6caae0060c76a4084d33887f7586f50aa9
Nicholai Bush <nbush@thedmsgrp.com>
2018-08-13 15:41:20 -0400

    [Eng-436] Enhancer almost ready, debugging config

---
+++ Integration/AbstractNeustarIntegration.php
@@ -0,0 +1,223 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nbush
+ * Date: 8/8/18
+ * Time: 11:07 AM
+ */
+namespace MauticPlugin\MauticEnhancerBundle\Integration;
+
+use GuzzleHttp\Client;
+use Mautic\LeadBundle\Entity\Lead;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+
+/**
+ * Class AbstractNeustarEnhancerIntegration
+ */
+abstract class AbstractNeustarIntegration extends AbstractEnhancerIntegration
+{
+    use NonFreeEnhancerTrait {
+        appendToForm as appendNonFree;
+    }
+
+    const NEUSTAR_PREFIX = 'neustar';
+
+    //const NEUSTAR_ENDPOINT_PROD = 'https://webgwy.targusinfo.com/access/query';
+
+    const NEUSTAR_ENDPOINT_DEV = 'https:/gwydemo.targusinfo.com/access/query';
+
+    protected static $serviceKeysDict = [
+        '1' => [
+            'format' => '/^\d{10}$/',
+            'desc' => 'mautic.enhancer.neustar.keys.phone.primary',
+            'alias' => 'lead.phone',
+        ],
+        '2' => [
+            'format' => '/^\d{10}$/',
+            'desc' => 'mautic.enhancer.neustar.keys.phone.secondary',
+            //'alias' => '',
+        ],
+        '572' => [
+            'desc' => 'mautic.enhancer.neustar.keys.email',
+            'alias' => 'lead.email',
+        ],
+        '574' => [
+            'format' => '/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/', //in UTC
+            'desc' => 'mautic.enhancer.neustar.keys.date',
+            'alias' => 'lead.date_added',
+        ],
+        '875' => [
+            'desc' => 'mautic.enhancer.neustar.keys.scoring_xml',
+        ],
+        '1390'=> [
+            'desc' => 'mautic.enhancer.neustar.keys.address.street',
+            'alias' => ['lead.address1','lead.address2'],
+        ],
+        '1391'=> [
+            'desc' => 'mautic.enhancer.neustar.keys.address.city',
+            'alias' => 'lead.city',
+        ],
+        '1392'=> [
+            'desc' => 'mautic.enhancer.neustar.keys.address.state',
+            'alias' => 'lead.state',
+        ],
+        '1393'=> [
+            'desc' => 'mautic.enhancer.neustar.keys.address.zip_code',
+            'alias' => 'lead.zipcode',
+        ],
+        '1395' => [
+            'required' => true,
+            'format'   => '/^\w+,\w+(,\w)?$/',                       // (,\w)?$/', //Last,First,MiddleIn
+            'desc'     => 'mautic.enhancer.neustar.keys.name',
+            'alias'    => ['lead.lastname', 'lead.firstname', ['substr', 'extendedField.middlename', 0, 1]]
+        ],
+        '3251' => [
+            'required' => true,
+        ],
+        '3256' => [
+            'required' => true,
+            'alias'    => ['setting.mkChannel', 'setting.mkVendorId', 'extendedField.military']
+        ]
+    ];
+
+    abstract protected function getNeustarElementId();
+    /**
+     * @return string
+     */
+    abstract protected function getNeustarIntegrationName();
+
+    /**
+     * Returns a list of keys use to build the scoring query
+     *
+     * @return array
+     */
+    abstract protected function getNeustarServiceKeys();
+
+    /**
+     * @return array
+     */
+    abstract protected function getAvailableResponses();
+
+    /**
+     * @param Lead lead
+     * @param int
+     *
+     * @return string
+     */
+    abstract protected function getServiceData(Lead $lead, $serviceId);
+
+    abstract protected function processResponse($neustarResponse);
+
+    /**
+     * @return array
+     */
+    public function getRequiredKeyFields()
+    {
+        return [
+            'username'   => 'mautic.enhancer.neustar.required_key.username',
+            'password'   => 'mautic.enhancer.neustar.required_key.password',
+            'serviceId' => 'mautic.enhancer.neustar.required_key.serviceId',
+        ];
+    }
+
+    public function getEnhancerFieldArray()
+    {
+        $fields = [];
+        foreach($this->getAvailableResponses() as $section => $attribues) {
+            foreach ($attribues as $attribute => $options) {
+                if (!is_array($options)) {
+                    $options = [$options];
+                }
+                foreach($options as $option) {
+                    $name = [self::NEUSTAR_PREFIX, $this->getElementId(), $section, $option];
+                    $fieldName = strtolower(implode('_', $name));
+                    $fields[$fieldName] = [
+                        'label' => ucfirst(implode(' ', $name)),
+                    ];
+                }
+            }
+        }
+        return $fields;
+    }
+
+    /**
+     * @param \Mautic\PluginBundle\Integration\Form|\Symfony\Component\Form\FormBuilder $builder
+     * @param array $data
+     * @param string $formArea
+     */
+    public function appendToForm(&$builder, $data, $formArea)
+    {
+        if ('features' === $formArea) {
+            $builder
+                ->add(
+                    'endpoint',
+                    UrlType::class,
+                    [
+                        'label' => $this->translator->trans('mautic.enhancer.neustar.query.endpoint.label'),
+                        'data'  => isset($data['endpoint']) ? $data['endpoint'] : '',
+                        'required' => true,
+                        'empty_value' => false,
+                        'label_attr' => ['class' => 'control-label'],
+                        'attr' => [
+                            'class' => 'form-control',
+                            'tooltip' => $this->translator->trans('mautic.enhancer.neustar.query.endpoint.tooltip'),
+                        ],
+
+                    ]
+                );
+        }
+        $this->appendNonFree($builder, $data, $formArea);
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return strtolower(self::NEUSTAR_PREFIX.$this->getNeustarIntegrationName());
+    }
+
+    /**
+     * @return string
+     */
+    public function getDisplayName()
+    {
+        return ucwords(sprintf('%s %s Scoring', self::NEUSTAR_PREFIX, $this->getNeustarIntegrationName()));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getAuthenticationType()
+    {
+        return 'None';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function doEnhancement(Lead &$lead)
+    {
+        $keys = $this->getKeys();
+
+        $query = [
+            'username' => $keys['username'],
+            'password' => $keys['password'],
+            'serviceId' => $keys['serviceId'],
+            'elems' => $this->getElementId(),
+            'version' => '1.0',
+            'transid' => '1',
+        ];
+
+        foreach ($this->getServiceKeys() as $serviceKey) {
+            $query['key'.$serviceKey] = $this->getServiceData($lead, $serviceKey);
+        }
+
+        $settings = $this->getIntegrationSettings()->getFeatureSettings();
+
+        $neustarClient = new Client();
+        $neustarResponse = $neustarClient->request('GET', $settings['endpoint'], ['query' => $query]);
+        $this->processResponse($neustarResponse);
+    }
+}
---
+++ Integration/Neustar3226Integration.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nbush
+ * Date: 8/8/18
+ * Time: 10:08 AM
+ */
+
+namespace MauticPlugin\MauticEnhancerBundle\Integration;
+
+use Mautic\LeadBundle\Entity\Lead;
+
+class Neustar3226Integration extends AbstractNeustarIntegration
+{
+    protected function getNeustarElementId()
+    {
+        return '3226';
+    }
+
+    protected function getNeustarIntegrationName()
+    {
+        return 'mpic';
+    }
+
+    /**
+     * @return array
+     */
+    protected function getNeustarServiceKeys()
+    {
+        return ['875'];
+    }
+
+    protected function getAvailableResponses()
+    {
+        return [
+            'address' => ['score'],
+            'phone' => ['score', 'appends' => ['validation', 'mobile']],
+            'email' => ['score'],
+        ];
+    }
+
+    protected function getServiceData(Lead $lead, $serviceId = '875')
+    {
+        // I would like to enable campaign config for the fields (phone, email, address, IP)
+        // and the append options (see Element ID 3226.pdf.
+        // for now, just address[score]
+        // $settings = $this->getIntegrationSettings()->getFeatureSettings();
+
+        $xmlDoc = new \DOMDocument('1.0');
+        $xmlDoc->preserveWhiteSpace = false;
+
+        $root = $xmlDoc->createElement('Contact');
+
+        $name = $xmlDoc->createElement('Name');
+        $name->setAttribute('type', 'C');
+        $name->appendChild($xmlDoc->createElement('First', $lead->getFirstname()));
+        $name->appendChild($xmlDoc->createElement('Last', $lead->getLastname()));
+
+        $names = $xmlDoc->createElement('Names')->appendChild($name);
+        $root->appendChild($names);
+
+        if ($lead->getAddress1()) {
+            $address = $xmlDoc->createElement('Address');
+            $address->setAttribute('score', '1');
+            $address->appendChild($xmlDoc->createElement('Street', $lead->getAddress1()));
+            $address->appendChild($xmlDoc->createElement('City', $lead->getCity()));
+            $address->appendChild($xmlDoc->createElement('ST', $lead->getState()));
+            $address->appendChild($xmlDoc->createElement('postal', $lead->getZipcode()));
+
+            $addresses = $xmlDoc->createElement('Addresses')->appendChild($address);
+            $root->appendChild($addresses);
+        }
+
+        if ($lead->getPhone()) {
+            $phone = $xmlDoc->createElement('Phone', $lead->getPhone());
+            $phone->setAttribute('score', '1');
+            $phone->setAttribute('appends', 'validation,mobile');
+
+            $phones = $xmlDoc->createElement('Phones')->appendChild($phone);
+            $root->appendChild($phones);
+        }
+
+        if ($lead->getEmail()) {
+            $email = $xmlDoc->createElement('eMail', $lead->getEmail());
+            $email->setAttribute('score', '1');
+
+            $emails = $xmlDoc->createElement('eMailAddresses')->appendChild($email);
+            $root->appendChild($emails);
+        }
+
+        $xmlDoc->appendChild($root);
+
+        return $xmlDoc->saveXML();
+    }
+
+    protected function processResponse($response)
+    {
+        return;
+    }
+}
