5caf3032a5bdbfd1d97dd145abe3d0bf971c7de7
Nicholai Bush <nicholaibush@yahoo.com>
2018-10-01 11:02:48 -0400

    [ENG-567] Updates Neustar MPIC to include phone[active]

--- MauticEnhancerBundle.php
+++ MauticEnhancerBundle.php
@@ -11,11 +11,23 @@
 
 namespace MauticPlugin\MauticEnhancerBundle;
 
+use Doctrine\DBAL\Schema\Schema;
+use Mautic\CoreBundle\Factory\MauticFactory;
 use Mautic\PluginBundle\Bundle\PluginBundleBase;
+use Mautic\PluginBundle\Entity\Plugin;
+use MauticPlugin\MauticEnhancerBundle\Integration\AbstractEnhancerIntegration;
 
 /**
  * Class MauticEnhancerBundle.
  */
 class MauticEnhancerBundle extends PluginBundleBase
 {
+    public static function onPluginUpdate(Plugin $plugin, MauticFactory $factory, $metadata = null, Schema $installedSchema = null)
+    {
+        /** @var AbstractEnhancerIntegration $integration */
+        foreach ($plugin->getIntegrations() as $integration) {
+            $stop = 'here';
+            $integration->buildEnhancerFields();
+        }
+    }
 }
