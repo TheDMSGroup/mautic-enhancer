fa6d6f912fb5fbe0bb2797ac07e336977a9ce090
Nicholai Bush <nbush@thedmsgrp.com>
2019-03-21 08:11:52 -0400

    [ENG-840] skeletons for remaining integrations

---
+++ Tests/Integration/CorrectAddressIntegrationTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nbush
+ * Date: 3/21/19
+ * Time: 8:05 AM.
+ */
+
+namespace MauticPlugin\MauticEnhancerBundle\Tests\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+class CorrectAddressIntegrationTest extends TestCase
+{
+}
---
+++ Tests/Integration/XverifyIntegrationTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nbush
+ * Date: 3/21/19
+ * Time: 8:10 AM.
+ */
+
+namespace MauticPlugin\MauticEnhancerBundle\Tests\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+class XverifyIntegrationTest extends TestCase
+{
+}
