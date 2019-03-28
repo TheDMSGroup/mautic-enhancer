aaeb222fac9dc005d2c029f882da545c2b51b3c3
Nicholai Bush <nbush@thedmsgrp.com>
2019-03-28 09:48:37 -0400

    [ENG-840] GenderFromName test

--- Tests/Integration/GenderFromNameIntegrationTest.php
+++ Tests/Integration/GenderFromNameIntegrationTest.php
@@ -10,6 +10,7 @@ namespace MauticPlugin\MauticEnhancerBundle\Tests\Integration;
 
 use Mautic\LeadBundle\Entity\Lead;
 use MauticPlugin\MauticEnhancerBundle\Integration\GenderFromNameIntegration;
+use MauticPlugin\MauticEnhancerBundle\Model\GenderNameModel;
 use PHPUnit\Framework\TestCase;
 
 class GenderFromNameIntegrationTest extends TestCase
@@ -17,12 +18,36 @@ class GenderFromNameIntegrationTest extends TestCase
     public function testDoEnhancement()
     {
         $leadObserver = $this->getMockBuilder(Lead::class)
-            ->setMethods(['addUpdatedField', 'getFieldValue'])
+            ->setMethods(['addUpdatedField', 'getFieldValue', 'getFirstname'])
             ->getMock();
 
+        $leadObserver->expects($this->once())
+            ->method('getFieldValue')
+            ->with('gender');
+
+        $leadObserver->expects($this->once())
+            ->method('getfirstname')
+            ->willReturn('first');
+
+        $leadObserver->expects($this->once())
+            ->method('addupdatedField')
+            ->with('gender', 'X', null);
+
         $mockIntegration = $this->getMockBuilder(GenderFromNameIntegration::class)
-            ->setMethods([])
+            ->setMethods(['getIntegrationModel'])
             ->getMock();
+
+        $mockModel = $this->getMockBuilder(GenderNameModel::class)
+            ->setMethods(['getGender'])
+            ->getMock();
+
+        $mockModel->expects($this->once())
+            ->method('getGender')
+            ->willReturn('X');
+
+        $mockIntegration->expects($this->any())
+            ->method('getIntegrationModel')
+            ->willReturn($mockModel);
 
         $this->assertTrue($mockIntegration->doEnhancement($leadObserver), 'Unexpected result.');
     }
