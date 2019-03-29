d3124fd1fbb3b1e886298b1b2fb2278d9b6138db
Nicholai Bush <nbush@thedmsgrp.com>
2019-03-29 10:42:34 -0400

    [ENG-840] Preliminary Neustar tests

--- Tests/Integration/AbstractNeustarIntegrationTest.php
+++ Tests/Integration/AbstractNeustarIntegrationTest.php
@@ -9,6 +9,7 @@
 namespace MauticPlugin\MauticEnhancerBundle\Tests\Integration;
 
 use Mautic\LeadBundle\Entity\Lead;
+use Mautic\PluginBundle\Entity\Integration;
 use MauticPlugin\MauticEnhancerBundle\Integration\AbstractNeustarIntegration;
 use PHPUnit\Framework\TestCase;
 
@@ -16,15 +17,46 @@ class AbstractNeustarIntegrationTest extends TestCase
 {
     public function testDoEnhancement()
     {
-        $this->markTestSkipped('WIP');
-
         $leadObserver = $this->getMockBuilder(Lead::class)
             ->setMethods(['addUpdatedField', 'getFieldValue'])
             ->getMock();
 
         $mockIntegration = $this->getMockBuilder(AbstractNeustarIntegration::class)
-            ->setMethods([])
+            ->setMethods(['getKeys', 'getNeustarElementId', 'getNeustarServiceKeys', 'getServiceIdData', 'getIntegrationSettings', 'processResponse', 'getNeustarIntegrationName', 'getEnhancerFieldArray'])
             ->getMock();
+
+        $mockSettings = $this->getMockBuilder(Integration::class)
+            ->setMethods(['getFeatureSettings'])
+            ->getMock();
+
+        $mockSettings->expects($this->any())
+            ->method('getFeatureSettings')
+            ->willReturn(['endpoint' => 'https://example.com']);
+
+        $mockIntegration->expects($this->any())
+            ->method('getIntegrationSettings')
+            ->willReturn($mockSettings);
+
+        $keys = ['username' => 'test', 'password' => 'test', 'serviceId' => 'test'];
+        $mockIntegration->expects($this->once())
+            ->method('getKeys')
+            ->willReturn($keys);
+
+        $mockIntegration->expects($this->once())
+            ->method('getNeustarElementId')
+            ->willReturn(7);
+
+        $mockIntegration->expects($this->once())
+            ->method('getNeustarServiceKeys')
+            ->willReturn([]);
+
+        $mockIntegration->expects($this->any())
+            ->method('getServiceIdData')
+            ->willReturn([]);
+
+        $mockIntegration->expects($this->once())
+            ->method('processResponse')
+            ->willReturn(true);
 
         $this->assertTrue($mockIntegration->doEnhancement($leadObserver), 'Unexpected result.');
     }
